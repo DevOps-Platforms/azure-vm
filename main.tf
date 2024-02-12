99bf15d095763b7728ceac00cfb9854780360f84
jozneto <j.a.neto@live.com>
2024-02-12 19:53:10 -0300

    inserting scripts sh and terraform file

--- main.tf
+++ main.tf
@@ -31,11 +31,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name   = azurerm_resource_group.vm.name
   location              = azurerm_resource_group.vm.location
   size                  = "Standard_DS1_v2"
-  admin_username        = var.azure_admin_user
+  admin_username        = "${var.DEVOPS_AZURE_ADMIN_USER}"
   network_interface_ids = [azurerm_network_interface.nic[count.index].id]
   admin_ssh_key {
-    username   = var.azure_admin_user
-    public_key = var.azure_ssh_public_key  
+    username   = "${var.DEVOPS_AZURE_ADMIN_USER}"
+    public_key = "${var.DEVOPS_AZURE_PUBLIC_SSH}"  
   }
 }
 
