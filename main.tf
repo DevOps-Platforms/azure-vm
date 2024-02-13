9437b79ff0ea47d9e7f5de9f350d43b4446a6f1f
jozneto <j.a.neto@live.com>
2024-02-13 16:52:00 -0300

    test bastion

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "vm-network" {
 
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "vm-subnet"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.vm.name
   virtual_network_name = azurerm_virtual_network.vm-network.name
   address_prefixes     = ["10.0.1.0/24"]
