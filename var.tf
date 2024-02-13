7420ceeba40ba067dbc12c2db050a44b8502b037
jozneto <j.a.neto@live.com>
2024-02-13 14:11:45 -0300

    test

--- var.tf
+++ var.tf
@@ -1,11 +1,11 @@
 variable "DEVOPS_AZURE_ADMIN_USER" {
   description = "Admin username"
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "DEVOPS_AZURE_PUBLIC_SSH" {
   description = "Public SSH key"
   type        = string
-  default     = ""
+  default     = null
 }
