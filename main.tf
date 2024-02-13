b77d1bacdff80c89eb0789920c05a8ce09853633
jozneto <j.a.neto@live.com>
2024-02-13 14:04:34 -0300

    test

--- main.tf
+++ main.tf
@@ -43,6 +43,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     name                 = "os_disk_name"
     storage_account_type = "Standard_LRS"
   }
+   source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
 }
 
 
