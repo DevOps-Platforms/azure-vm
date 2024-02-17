351f29d1416c7a7d27a6509a789d3a4f5ebe471f
jozneto <j.a.neto@live.com>
2024-02-17 00:23:22 -0300

    test2

--- modules/azure/azure_vm.tf
+++ modules/azure/azure_vm.tf
@@ -25,11 +25,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
   resource_group_name   = each.value.rg
   location              = each.value.location
   size                  = each.value.size
-  admin_username        = var.DEVOPS_AZURE_ADMIN_USER
+  admin_username        = var.adminuser
   network_interface_ids = [azurerm_network_interface.nic[each.key].id]
   admin_ssh_key {
-    username   = var.DEVOPS_AZURE_ADMIN_USER
-    public_key = var.DEVOPS_AZURE_PUBLIC_SSH
+    username   = var.adminuser
+    public_key = var.ssh
   }
    os_disk {
     caching              = each.value.caching
