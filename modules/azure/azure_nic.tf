91b646e8bdd6a677fa99ef80bdc6abaa64f160f3
jozneto <j.a.neto@live.com>
2024-02-19 17:53:01 -0300

    change creds file

--- modules/azure/azure_nic.tf
+++ modules/azure/azure_nic.tf
@@ -9,6 +9,24 @@ variable "nics" {
   }))
 }
 
+# resource "azurerm_network_interface" "nic" {
+#   depends_on = [
+#     azurerm_resource_group.vm, 
+#     azurerm_public_ip.ips
+#   ]
+  
+#   for_each = var.nics
+#   name                            = each.value.name
+#   location                        = each.value.location
+#   resource_group_name             = each.value.rg
+
+#   ip_configuration {
+#     name                          = each.value.ipconfigname
+#     subnet_id                     = azurerm_subnet.general.id
+#     private_ip_address_allocation = each.value.privip
+#     public_ip_address_id          = azurerm_public_ip.ips.id
+#   }
+# }
 resource "azurerm_network_interface" "nic" {
   depends_on = [
     azurerm_resource_group.vm, 
@@ -24,6 +42,8 @@ resource "azurerm_network_interface" "nic" {
     name                          = each.value.ipconfigname
     subnet_id                     = azurerm_subnet.general.id
     private_ip_address_allocation = each.value.privip
-    public_ip_address_id          = azurerm_public_ip.ips.id
+    
+    public_ip_address_id = can(var.ips[each.key]) && can(azurerm_public_ip.ips[each.key]) ? azurerm_public_ip.ips[each.key].id : null
   }
 }
+
