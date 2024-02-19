27182f9f7ef7851d0d976410313dd698a0cf956a
jozneto <j.a.neto@live.com>
2024-02-19 17:37:56 -0300

    change creds file

--- modules/azure/output.tf
+++ modules/azure/output.tf
@@ -1,6 +1,3 @@
-output "public_ip_for_instance" {
-  value = {
-    for key, value in azurerm_public_ip.ips : 
-    key => value.ip_address if contains(keys(var.ips), key) && var.ips[key].name == value.name
-  }
+output "public_ips" {
+  value = azurerm_public_ip.ips[*].ip_address
 }
