c3e3aff20b033179ff14563744e079f3f890b2d0
jozneto <j.a.neto@live.com>
2024-02-19 17:55:37 -0300

    change creds file

--- modules/azure/azure_nic.tf
+++ modules/azure/azure_nic.tf
@@ -43,7 +43,7 @@ resource "azurerm_network_interface" "nic" {
     subnet_id                     = azurerm_subnet.general.id
     private_ip_address_allocation = each.value.privip
     
-    public_ip_address_id = can(var.ips[each.key]) && can(azurerm_public_ip.ips[each.key]) ? azurerm_public_ip.ips[each.key].id : null
+    public_ip_address_id = can(azurerm_public_ip.ips[each.key]) ? azurerm_public_ip.ips[each.key].id : null
   }
 }
 
